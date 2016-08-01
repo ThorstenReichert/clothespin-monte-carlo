3aad91e032718d4df4b78b67846297c82086deb8
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2016-08-01 19:30:59 +0200

    updated config to produce nice results

--- config.js
+++ config.js
@@ -4,31 +4,46 @@ module.exports = function (wagner) {
 
     const config = {};
 
+    config.node = {};
+
+    // number of worker threads
+    config.node.concurrency = 4;
+
     config.sim = {};
 
     // binning width for results
-    config.sim.bin = 0.5;
+    config.sim.bin = 0.01;
 
     // length of washing line
     config.sim.length = 100.0;
 
     // number of pins
-    config.sim.pins = 43;
+    config.sim.pins = 22;
 
-    // # or monte-carlo iterations
-    config.sim.steps = 1000;
+    // # or monte-carlo iterations per thread
+    config.sim.steps = 1000000;
 
     // half-width of clothespins
-    config.sim.width = 1;
+    config.sim.width = 2;
 
     config.gfx = {};
 
-    // width of the result graph
+    // width of console output
     config.gfx.width = 100;
+
+    // percentage of completion after which status is updated
+    config.gfx.update = 10;
+
+    config.gpl = {};
+
+    // display style
+    config.gpl.style = 'lines';
 
 
     wagner.factory('Config', function () {
         return config;
     });
+
+    return config;
 
 };
