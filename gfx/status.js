60e4192a97f004022ffd5b50810e184d21817ff6
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2016-08-01 19:31:52 +0200

    added progress bars

---
+++ gfx/status.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const clear = require('cli-clear');
+const max = require('../util/max');
+const init = require('../util/init-array');
+
+module.exports = function (wagner) {
+
+    return wagner.invoke(function (Config) {
+
+        return function (progress) {
+            clear();
+
+            console.log('Currently pinning ');
+            console.log(' - ' + Config.sim.pins + ' pins of width ' + Config.sim.width);
+            console.log(' - onto a line of length ' + Config.sim.length);
+            console.log(' - over ' + Config.node.concurrency + ' * ' + Config.sim.steps + ' iterations');
+            console.log('');
+            console.log('');
+            console.log('progress per worker:');
+            console.log('');
+
+            for(let i = 0; i < Config.node.concurrency; i++) {
+                let rel = Math.floor((Config.gfx.width - 2) * progress[i]);
+                let s = '*' + Array(rel).join('|') + '*';
+                console.log(s);
+            }
+        };
+
+    });
+
+};
